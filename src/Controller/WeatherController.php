deac87c5a7e0ed1b4e3b630ee120c463b7fcafd3
thearti121 <artur11.2001@o2.pl>
2025-11-07 15:56:54 +0100

    lab_c_kompletny
    dodano wyszukiwanie po nazwie miasta.

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,21 +2,45 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Location;
-use App\Repository\MeasurementRepository;
+use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class WeatherController extends AbstractController
 {
-    #[Route('/weather/{city}/{country}', name: 'app_weather_by_name', requirements: ['country' => '[A-Z]{2}'])]
-    public function city(Location $location, MeasurementRepository $repository): Response
+    //po id
+    /*#[Route('/weather/{id}', name: 'app_weather_show')]
+    public function show(LocationRepository $locationRepository, int $id): Response
     {
-        $measurements = $repository->findByLocation($location);
+        $location = $locationRepository->find($id);
+
+        if (!$location) {
+            throw $this->createNotFoundException('Nie znaleziono lokalizacji o ID ' . $id);
+        }
+
         return $this->render('weather/city.html.twig', [
             'location' => $location,
-            'measurements' => $measurements,
+            'measurements' => $location->getMeasurements()
+        ]);
+    }*/
+
+    // po nazwie miasta.
+    #[Route('/weather/{city}', name: 'app_weather_show')]
+    public function show(LocationRepository $locationRepository, string $city): Response
+    {
+        $location = $locationRepository->findOneBy(['city' => $city]);
+
+        if (!$location) {
+            throw $this->createNotFoundException('Nie znaleziono lokalizacji o nazwie ' . $city);
+        }
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $location->getMeasurements(),
         ]);
     }
+
 }
